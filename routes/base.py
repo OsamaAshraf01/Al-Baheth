4434d6e8bcde8ee9cde12a3ce200a7fc048c9825
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-14 05:54:52 +0200

    Create Base router and main file

---
+++ routes/base.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from helpers.config import get_settings, Settings
+
+base_router = APIRouter(
+    prefix='/api/v1',
+    tags=["api_v1", "base"]
+)
+
+@base_router.get('/')
+async def welcome(app_settings: Settings = Depends(get_settings)):
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+    allowed_files = app_settings.FILE_ALLOWED_TYPES
+    max_size = app_settings.FILE_MAX_SIZE
+
+    return {
+        "app_name": app_name,
+        "app_version": "v" + app_version,
+        "allowed_types": allowed_files,
+        "max_size": str(max_size) + "Mb"
+    }

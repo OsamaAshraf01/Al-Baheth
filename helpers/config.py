b5b91441f9aa0a6c3ad23258b4c7f07bab8f2524
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-14 05:55:02 +0200

    Add config file

---
+++ helpers/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+import os
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    APP_VERSION: str
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
+
+    
+    model_config = {
+        "env_file": os.path.join(os.path.dirname(__file__), "../.env"),
+        "env_file_encoding": "utf-8",
+        "extra": "ignore"
+    }
+
+
+def get_settings():
+    return Settings()

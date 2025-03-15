592549cdc9dcd585fc1b5695e18702534111900e
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-15 18:27:59 +0200

    download needed from NLTK

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes import base, upload, processing
+from nltk import download
+download('stopwords')
+download('wordnet')
+download('punkt')
+download('punkt_tab')
+download('averaged_perceptron_tagger')
+download('averaged_perceptron_tagger_eng')
+
 
 app = FastAPI()
 app.include_router(base.base_router)
 app.include_router(upload.upload_router)
-# app.include_router(processing.processing_router)
+app.include_router(processing.processing_router)
 

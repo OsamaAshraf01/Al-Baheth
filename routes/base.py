aa64c7db08ded21e868fb907f2849d8fcd6c80d9
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-15 18:28:30 +0200

    remove misplaced part

--- routes/base.py
+++ routes/base.py
@@ -20,7 +20,3 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
         "allowed_types": allowed_files,
         "max_size": str(max_size) + "Mb"
     }
-
-@base_router.get('/Preprocess/{file_name}')
-async def preprocess(file_name: str, controller: ProcessingController = Depends()):
-    return await controller.preprocess(file_name)

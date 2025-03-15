4d1cffbb852ebabfc20799c25899bd49bf2c1fac
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-03-16 00:43:20 +0200

    editting indexing controller

--- controllers/IndexingController.py
+++ controllers/IndexingController.py
@@ -5,6 +5,8 @@ import os
 import pyterrier as pt
 import requests
 from .BaseController import BaseController
+from controllers.ProcessingController import ProcessingController
+import pandas as pd
 
 class IndexingController(BaseController):
     def __init__(self):
@@ -16,17 +18,19 @@ class IndexingController(BaseController):
     def _get_file_path(self, file_name):
         return os.path.join(self.files_dir, file_name)
 
-    def _preprocess_file(self, file_name: str) -> str:
+    def _preprocess_file(self, file_name: str, controller : ProcessingController = Depends()) -> str:
         """
         Preprocess a file using the /preprocess/{file_name} endpoint.
         
         :param file_name: Name of the file to preprocess.
         :return: Preprocessed text content.
         """
-        response = requests.get(f"http://localhost:8000/api/v1/processing/preprocess/{file_name}")
-        if response.status_code != status.HTTP_200_OK:
-            raise HTTPException(status_code=response.status_code, detail=response.json())
-        return response.json().get("Processed Text", "")
+        
+        # response = requests.get(f"http://localhost:8000/api/v1/processing/preprocess/{file_name}")
+        # if response.status_code != status.HTTP_200_OK:
+        #    raise HTTPException(status_code=response.status_code, detail=response.json())
+        # return response.json().get("Processed Text", "")
+        return controller.preprocess(file_name)
 
     def _index_files(self, file_names: list):
         """
@@ -34,7 +38,7 @@ class IndexingController(BaseController):
         
         :param file_names: List of file names to index.
         """
-        indexer = pt.IterDictIndexer(self.index_dir, overwrite=True)
+        indexer = pt.DFIndexer(self.index_dir, overwrite=True)
         docs = []
         for file_name in file_names:
             preprocessed_text = self._preprocess_file(file_name)
@@ -42,7 +46,10 @@ class IndexingController(BaseController):
                 'docno': file_name,
                 'text': preprocessed_text
             })
-        index_ref = indexer.index(docs)
+        docs = pd.DataFrame(docs)
+        docs['text'] = docs['text'].astype(str)
+        docs['docno'] = docs['docno'].astype(str)
+        index_ref = indexer.index(docs['text'], docs['docno'])
         return index_ref
 
     def index_all_files(self):

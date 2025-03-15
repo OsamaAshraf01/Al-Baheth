2766cd31811b5f92af67b45fc9c5e1af6751044e
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-15 15:38:30 +0200

    Enhancing responses

--- controllers/UploadController.py
+++ controllers/UploadController.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import os
 from .BaseController import BaseController
+from fastapi.responses import JSONResponse
+from fastapi import status
 
 class UploadController(BaseController):
-    
     async def upload(self, file: UploadFile = File(...)):
         file_path = os.path.join(self.files_dir, file.filename)
         try:
             with open(file_path, "wb") as f:
                 f.write(await file.read())
         except:
-            raise HTTPException(status_code=400, detail="Failed to upload the file")
+            raise JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                content={
+                    "Message" : "Failed to upload the file"
+                }
+            )
         
-        return {"Message": "Successfully uploaded"}
+        return JSONResponse(
+            status_code=200,
+            content={
+                "Message": "Successfully uploaded"
+            }
+        )

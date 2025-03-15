ed679838a6a968fbb0bebf60dc0bc54ea42f8cb7
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-15 15:42:06 +0200

    Correcting route including

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from routes import base
+from routes import base, upload
 
 app = FastAPI()
 app.include_router(base.base_router)
+app.include_router(upload.upload_router)
 
--- routes/base.py
+++ routes/base.py
@@ -8,8 +8,6 @@ base_router = APIRouter(
     tags=["api_v1", "base"]
 )
 
-base_router.include_router(upload_route)
-
 @base_router.get('/')
 async def welcome(app_settings: Settings = Depends(get_settings)):
     app_name = app_settings.APP_NAME

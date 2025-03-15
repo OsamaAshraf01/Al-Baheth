efde1887520996bcbbd35f926a99f41ec8456454
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-15 16:17:48 +0200

    commenting processing route initially

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from routes import base, upload, processing
 app = FastAPI()
 app.include_router(base.base_router)
 app.include_router(upload.upload_router)
-app.include_router(processing.processing_router)
+# app.include_router(processing.processing_router)
 

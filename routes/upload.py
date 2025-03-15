16e932768168d1efa37e587d2ac950b845988d89
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-15 15:41:41 +0200

    improve prefix and routes organization

--- routes/upload.py
+++ routes/upload.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends
 from controllers.UploadController import UploadController, UploadFile, File
 
-upload_route = APIRouter(
-    prefix= "/upload",
-    tags= ["upload"]
+upload_router = APIRouter(
+    prefix= "/api/v1/upload",
+    tags= ["api_v1", "upload"]
 )
 
-@upload_route.post("/")
+@upload_router.post("/")
 async def upload(file : UploadFile = File(...), controller : UploadController = Depends()):
     return await controller.upload(file)

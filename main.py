54649f207d6781a3e848e53d51187af311834c59
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-03-15 15:43:18 +0200

    include processing router

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routes import base, upload
+from routes import base, upload, processing
 
 app = FastAPI()
 app.include_router(base.base_router)
 app.include_router(upload.upload_router)
+app.include_router(processing.processing_router)
 

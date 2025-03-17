be507f2aa95c04f2fcebb15e0ef716e2c4f7e4c4
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-03-17 16:05:10 +0200

    Move NLTK data downloads to setup.py for better package management

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI
 from routes import base_router, upload_router, processing_router, indexing_router
-from nltk import download
-download('stopwords')
-download('wordnet')
-download('punkt')
-download('punkt_tab')
-download('averaged_perceptron_tagger')
-download('averaged_perceptron_tagger_eng')
-
 
 app = FastAPI()
 app.include_router(base_router)
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from helpers.config import get_settings
+import nltk
+
+def download_nltk_data():
+    nltk.download("stopwords")
+    nltk.download("wordnet")
+    nltk.download("punkt")
+    nltk.download("punkt_tab")
+    nltk.download("averaged_perceptron_tagger")
+    nltk.download("averaged_perceptron_tagger_eng")
+
+download_nltk_data()
+
+with open("requirements.txt") as f:
+    REQUIREMENTS_TXT = f.read().splitlines()
+
+setup(
+    name= "Al-Baheth",
+    packages= find_packages(),
+    install_requires= REQUIREMENTS_TXT,
+    version= get_settings().APP_VERSION,
+    include_package_data=True,
+    url="https://github.com/OsamaAshraf01/Al-Baheth",
+)

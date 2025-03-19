a0993a0eff72a84cc863640576529e60b72abf89
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-03-19 18:12:06 +0200

    fix a bug

--- controllers/BaseController.py
+++ controllers/BaseController.py
@@ -3,7 +3,7 @@ import os
 
 # this controller will contain the common methods that will be used by other controllers
 class BaseController:
-    def __init__(self, settings : Settings):
-        self.app_settings = settings
+    def __init__(self):
+        self.app_settings = Settings()
         self.base_dir = os.path.dirname(os.path.dirname(__file__))  # To get the dir of src folder
         self.files_dir = os.path.join(self.base_dir, "assets")

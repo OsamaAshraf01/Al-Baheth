226caf4217bbcad6f670f31cd20ce75c461fd0d2
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-03-19 17:54:09 +0200

    refactor: update service annotations in services.__init__.py to use Depends for improved dependency injection

--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,8 @@
 from typing_extensions import Annotated
+from fastapi import Depends
 from .PDF import PDFReaderService as PDFReaderServiceClass
 from .Language import LanguageService as LanguageServiceClass
 from  .dependencies import getPDFReaderService, getLanguageService
 
-PDFReaderService = Annotated[PDFReaderServiceClass, getPDFReaderService]
-LanguageService = Annotated[LanguageServiceClass, getLanguageService]
+PDFReaderService = Annotated[PDFReaderServiceClass, Depends(getPDFReaderService)]
+LanguageService = Annotated[LanguageServiceClass, Depends(getLanguageService)]

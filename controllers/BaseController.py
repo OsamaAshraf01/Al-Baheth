c673987bf24ff1f6b4fe0010f9f396365c45ceb5
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-03-19 18:08:19 +0200

    make use of Annotated Settings

--- controllers/BaseController.py
+++ controllers/BaseController.py
@@ -1,9 +1,9 @@
-from helpers.config import get_settings
+from helpers import Settings
 import os
 
 # this controller will contain the common methods that will be used by other controllers
 class BaseController:
-    def __init__(self):
-        self.app_settings = get_settings()
+    def __init__(self, settings : Settings):
+        self.app_settings = settings
         self.base_dir = os.path.dirname(os.path.dirname(__file__))  # To get the dir of src folder
         self.files_dir = os.path.join(self.base_dir, "assets")

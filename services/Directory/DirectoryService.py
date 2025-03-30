7616a50b899af0369cb8a20917b37b6e04bb75b5
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-03-30 12:40:50 +0200

    add index directory path to DirectoryService

--- services/Directory/DirectoryService.py
+++ services/Directory/DirectoryService.py
@@ -4,3 +4,4 @@ class DirectoryService:
     # static atribute
     base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     files_dir = os.path.join(base_dir, "assets")
+    index_dir = os.path.join(base_dir, "index")

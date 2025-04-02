c8ef3fa37fa2c7e07b9103eab34e5c668c7fe982
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-04-02 14:50:15 +0200

    refactor: replace upload and processing routers with files router in main application

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from routes import base_router, upload_router, processing_router, indexing_router, query_router
+from routes import base_router, files_router, indexing_router, query_router
 
 app = FastAPI()
 app.include_router(base_router)
-app.include_router(upload_router)
-app.include_router(processing_router)
+app.include_router(files_router)
 app.include_router(query_router)
 app.include_router(indexing_router)
 

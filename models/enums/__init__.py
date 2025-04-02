f28c2bdffe827f0a34d8d53c81d2177ce2cc06d0
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-04-02 14:50:01 +0200

    refactor: remove unused PDFEnum import from enums module to streamline code

--- models/enums/__init__.py
+++ models/enums/__init__.py
@@ -1,6 +1,5 @@
 from .ProcessingEnum import ProcessingEnum
 from enum import Enum
-from .PDFEnum import PDFEnum
 from .LanguageProcessingEnum import LanguageProcessingEnum
 from .FileEnum import FileEnum
 from .IndexingEnum import IndexingEnum

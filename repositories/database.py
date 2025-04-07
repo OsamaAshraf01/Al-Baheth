3c7e437b3efd8bb5941a0e1a6a7ff9fa0672d89b
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-04-07 17:12:11 +0200

    feat: add database connection module for MongoDB integration

---
+++ repositories/database.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers import Settings
+from beanie import init_beanie
+
+async def db_connection(Settings: Settings):
+    
+    client = AsyncIOMotorClient(Settings.DB_URL)
+    await init_beanie(database=client.blog)

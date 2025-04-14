a4cab949cffcc8592dd9ee18ce23cf22e17a67c0
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-04-14 13:41:04 +0200

    refactor: replace db_connection with lifespan context manager for database handling

--- backend/core/helpers/Lifespan.py
+++ backend/core/helpers/Lifespan.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from repositories.database import db_connection
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
+from beanie import init_beanie
+from models import Document
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,5 +14,11 @@ async def lifespan(app: FastAPI):
     
     :param app: FastAPI application instance.
     """
-    await db_connection()
+    db_Settings = get_settings()
+    app.client = AsyncIOMotorClient(db_Settings.DB_URL)
+    app.db = app.client.al_baheth
+    await init_beanie(database=app.db, document_models=[Document])
+    
     yield
+    
+    app.client.close()
--- backend/core/repositories/database.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from helpers.config import Settings, get_settings
-from beanie import init_beanie
-from models import Document
-
-async def db_connection():
-    db_Settings: Settings = get_settings()
-    client = AsyncIOMotorClient(db_Settings.DB_URL)
-    await init_beanie(database=client.al_baheth, document_models=[Document])

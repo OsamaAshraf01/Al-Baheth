ae3dad65b711cbe3c70b0c36229647e4df3054cc
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-04-21 14:41:42 +0200

    feat: implement DocumentRepository with create method for document creation

---
+++ backend/core/repositories/DocumentRepository.py
@@ -0,0 +1,15 @@
+from models import File, Document
+import hashlib
+
+class DocumentRepository:
+
+    async def create(self, file: File, content : str) -> Document:
+        """
+        Create a new document in the database from the file.
+        
+        :param file: File object containing the file information.
+        :return: Document object created from the file.
+        """
+        hashed_content = hashlib.sha256(content.encode()).hexdigest()
+        document = Document(file=file.file, title=file.file.filename, hashed_content=hashed_content)
+        return await document.insert()

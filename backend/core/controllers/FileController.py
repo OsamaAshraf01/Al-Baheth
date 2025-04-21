7aeb9bd3396b8847590a569919f869c8265e3d72
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-04-21 02:21:40 +0200

    fix indent

--- backend/core/controllers/FileController.py
+++ backend/core/controllers/FileController.py
@@ -90,21 +90,21 @@ class FileController(BaseController):
         return self._clean(content)
     
     async def upload(self, file: File):
-    file_path = os.path.join(DirectoryService.files_dir, file.file.filename)
-    try:
-        with open(file_path, "wb") as f:
-            f.write(await file.file.read())
-    except:
-        raise JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST, 
+        file_path = os.path.join(DirectoryService.files_dir, file.file.filename)
+        try:
+            with open(file_path, "wb") as f:
+                f.write(await file.file.read())
+        except:
+            raise JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                content={
+                    "Message" : "Failed to upload the file"
+                }
+            )
+        
+        return JSONResponse(
+            status_code=200,
             content={
-                "Message" : "Failed to upload the file"
+                "Message": "Successfully uploaded"
             }
         )
-    
-    return JSONResponse(
-        status_code=200,
-        content={
-            "Message": "Successfully uploaded"
-        }
-    )

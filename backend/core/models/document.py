611eab311748751c1886eb0f7e116c5254049231
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-04-21 02:24:46 +0200

    refactor: add hashed_content field to Document model for unique content indexing

--- backend/core/models/document.py
+++ backend/core/models/document.py
@@ -1,9 +1,11 @@
 from fastapi import UploadFile
-from beanie import Document as BeanieDocument
+from typing_extensions import Annotated
+from beanie import Document as BeanieDocument, Indexed
 
 class Document(BeanieDocument):
     file: UploadFile
     title: str
+    hashed_content : Annotated[str, Indexed(unique=True)]
     
     
     class Settings:

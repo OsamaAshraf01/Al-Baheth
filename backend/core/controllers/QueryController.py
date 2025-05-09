ad72ac6d5b47221ca1e029b53ec3fa1b14dad11d
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-05-09 18:02:30 +0300

    Initial query controller

--- backend/core/controllers/QueryController.py
+++ backend/core/controllers/QueryController.py
@@ -1,6 +1,25 @@
-from ..controllers import BaseController
+from fastapi import Depends
+
+from ..controllers import BaseController, DataController
+from ..repositories import DocumentRepo
 
 
 class QueryController(BaseController):
-    async def query(self, query):
-        return {"query": query}
+    def __init__(
+            self,
+            document_repository: DocumentRepo,
+            data_controller: DataController = Depends()
+    ):
+        super().__init__()
+        self.data_controller = data_controller
+        self.document_repository = document_repository
+
+    async def query(self, query: str) -> list:
+        """
+        Search for documents in the index.
+
+        :param query: The search query.
+        :return: A list of document titles matching the search query.
+        """
+        query = self.data_controller.clean_text(query)
+        return await self.document_repository.search(query)

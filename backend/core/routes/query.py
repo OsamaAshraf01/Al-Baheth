66d9055d19ea65407bd5ca1474702643dc24b051
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2025-05-09 18:03:13 +0300

    Improving query endpoint for more detailed response

--- backend/core/routes/query.py
+++ backend/core/routes/query.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from starlette.responses import JSONResponse
 
-from ..controllers import FileController
+from ..controllers import QueryController
+from ..models.enums import ResponseEnum
 
 query_router = APIRouter(
     prefix="/api/v1/query",
@@ -9,5 +11,26 @@ query_router = APIRouter(
 
 
 @query_router.get("/")
-async def query(query: str, controller: FileController = Depends()):
-    return await controller.query(query)
+async def query(query: str, controller: QueryController = Depends()):
+    results = await controller.query(query)
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "summary" : {
+                "message": ResponseEnum.SEARCH_SUCCESS.value,
+                "query": query,
+                "results_count": len(results),
+                "results_list": [
+                    {
+                        f"rank {i}": {
+                            "title":doc['title'],
+                            "score":doc['score']
+                        }
+                    }
+                    for i, doc in zip(range(1, len(results) + 1), results)
+                ]
+            },
+            "results": results
+        }
+    )
+
